71c94cd6508150967e4196656ca4c9b31d2715b1
Yuran <it.yuranadolfo@gmail.com>
2026-01-27 17:04:20 +0200

    Entendendo a classe Math

---
+++ Java/Java I/Operacoes matematicas/Readme.md
@@ -0,0 +1,3 @@
+### Operações matemáticas
+
+A class **Math** contem metodos para operacoes numericas basicas comoa exponencial elementar, logaritmo, raiz quadrada, e funções trigonométricas
---
+++ Java/Java I/Operacoes matematicas/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int numero = 7;
+
+        int resultado = (int)Math.sqrt(49); //Fazendo o cast  do resultado para inteiro
+        System.out.println(resultado);
+
+        System.out.println(Math.max(4,3)); //Imprime o maior valor
+
+    }
+}

e6810e21b16dbb83024b16ae6a7677cbb6bcfe41
Yuran <it.yuranadolfo@gmail.com>
2026-02-03 17:54:51 +0200

    Emcapsulamento

---
+++ Java/Java I/Emcapsulamento/Readme.md
@@ -0,0 +1,3 @@
+# Emcapsulamento
+
+Emcapsulamento = Atributos de uma classe estarao escondidos ou privados, e podem ser <br> acessados apenas atraveis de metodos especificos (Getters e setters) 
---
+++ Java/Java I/Emcapsulamento/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Person person=new Person("Alfred", 32);
+
+        System.out.println(person.getName() );
+    }
+}
+
+
+
+
